09750948b9b56a1b71ec066861c037c0664dc132
李俊东 <lijundong@51zhaoyou.com>
2018-12-13 11:02:38 +0800

    dotars-init
    dotars-init

---
+++ src/DotarsInit.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace dotars
+
+class DotarsInit
+{
+	public static index()
+	{
+		$str = read();
+		addFile('./jiaohu.txt',$str);
+	}
+	public static function read($str = '请输入')
+	{
+		//提示输入
+		fwrite(STDOUT, $str . ":");
+		//获取用户输入数据
+		$result = trim(fgets(STDIN));
+		return trim($result);
+	}
+	public static function addFile($file_name,$content){
+		$myfile = fopen($file_name, "a+") or die("Unable to open file!");
+		fwrite($myfile, $content);
+		fclose($myfile);
+	}
+}
