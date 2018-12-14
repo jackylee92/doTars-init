7de3a5bfa7f6d0ee270b7f920e1ae1c550780db4
李俊东 <lijundong@51zhaoyou.com>
2018-12-14 19:55:51 +0800

    获取参数
    获取参数

--- src/DotarsInit.php
+++ src/DotarsInit.php
@@ -4,19 +4,37 @@ namespace dotars;
 
 class DotarsInit
 {
+
 	public static function index()
-	{
-		$str = self::read();
-		self::addFile('./jiaohu.txt',$str);
+    {
+        $param = [
+            'doTarsIP' => self::mustRead('主控IP'),
+            'doTarsServerName' => self::mustRead('服务名'),
+            'doTarsServantName' => self::mustRead('Servant名'),
+            'doTarsObjName' => self::mustRead('Obj名'),
+        ];
+		self::addFile('./param.txt',json_encode($param,JSON_UNESCAPED_UNICODE));
 	}
+
+    public static function mustRead($name='')
+    {
+        $tips = '请输入'.$name;
+        $data = self::read($tips);
+        while($data == '')
+        {
+            $tips = $name.'必填';
+            $data = self::read($tips);
+        }
+        return $data;
+    }
+
 	public static function read($str = '请输入')
 	{
-		//提示输入
 		fwrite(STDOUT, $str . ":");
-		//获取用户输入数据
 		$result = trim(fgets(STDIN));
 		return trim($result);
 	}
+
 	public static function addFile($file_name,$content){
 		$myfile = fopen($file_name, "a+") or die("Unable to open file!");
 		fwrite($myfile, $content);
