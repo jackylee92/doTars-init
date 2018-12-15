0a545604a6c337114fc28410de0ff64e436dc278
cloud-user <cloud-user@localhost.localdomain>
2018-12-15 04:11:12 -0500

    init

--- src/DotarsInit.php
+++ src/DotarsInit.php
@@ -6,28 +6,29 @@ class DotarsInit
 {
 
 	public static function index()
-    {
-        $param = [
-            'doTarsIP' => self::mustRead('主控IP'),
-            'doTarsType' => self::mustRead('类型(server\client)'),
-            'doTarsServerName' => self::mustRead('服务名'),
-            'doTarsServantName' => self::mustRead('Servant名'),
-            'doTarsObjName' => self::mustRead('Obj名'),
-        ];
+	{
+		$param = [
+			'doTarsIP' => self::mustRead('主控IP'),
+			'doTarsType' => self::mustRead('类型(server\client)'),
+			'doTarsServerName' => self::mustRead('服务名'),
+			'doTarsServantName' => self::mustRead('Servant名'),
+			'doTarsObjName' => self::mustRead('Obj名'),
+			];
 		self::addFile('./param.txt',json_encode($param,JSON_UNESCAPED_UNICODE));
+		echo shell_exec('./tmp/init.sh '.$param['doTarsIP'].' '.$param['doTarsType'].' '.$param['doTarsServerName'].' '.$param['doTarsServantName'].' '.$param['doTarsObjName']);
 	}
 
-    public static function mustRead($name='')
-    {
-        $tips = '请输入'.$name;
-        $data = self::read($tips);
-        while($data == '')
-        {
-            $tips = $name.'必填';
-            $data = self::read($tips);
-        }
-        return $data;
-    }
+	public static function mustRead($name='')
+	{
+		$tips = '请输入'.$name;
+		$data = self::read($tips);
+		while($data == '')
+		{
+			$tips = $name.'必填';
+			$data = self::read($tips);
+		}
+		return $data;
+	}
 
 	public static function read($str = '请输入')
 	{
