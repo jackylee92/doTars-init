e6f864e3197e46e1d8dbea9c3296b0064260400f
李俊东 <lijundong@51zhaoyou.com>
2018-12-19 10:04:09 +0800

    servant
    servant

--- src/DotarsInit.php
+++ src/DotarsInit.php
@@ -13,9 +13,8 @@ class DotarsInit
         $doTarsServantName = ucwords(self::mustRead('Servant名'));
         $doTarsObjName = ucwords(self::mustRead('Obj名'));
         if($doTarsType == 'server' && !self::validateFile('../'.$doTarsServerName.$doTarsServantName.'.tars')) {
-            echo $doTarsServerName.$doTarsServantName.'.tars 文件不存在，请定义在项目名同级！';
             shell_exec('rm -rf ./tmp composer.json');
-            exit();
+            exit($doTarsServerName.$doTarsServantName.'.tars 文件不存在，请定义在项目名同级！');
 
         }
         $commond = './tmp/init.sh '.$doTarsIP.' '.$doTarsType.' '.$doTarsServerName.' '.$doTarsServantName.' '.$doTarsObjName;
@@ -78,4 +77,53 @@ class DotarsInit
             return false;
         }
     }
+    public static function servant()
+    {
+        $servantFile = self::mustRead('Tars文件名');
+        $servantFile = self::servantName($servantFile);
+        if(!self::validateFile('../tars/'.$servantFile)) {
+            exit($servantFile.'文件不存在tars目录中！');
+        }
+        $servantPath = ucwords(self::mustRead('服务地址(Servant.Server.Obj)','validateServantPath'));
+        $servantArr = explode('.',$servantPath);
+        $commond = 'mkdir ../tars/'.$servantArr[0].$servantArr[1].' && touch ../tars/'.$servantArr[0].$servantArr[1].'/tars.proto.php && mv ../tars/'.$servantFile.' ../tars/'.$servantArr[0].$servantArr[1].'/';
+        shell_exec($commond);
+        $content = "<?php
+return array(
+    'appName' => '".$servantArr[0]."',
+    'serverName' => '".$servantArr[1]."',
+    'objName' => '".$servantArr[2]."',
+    'withServant' => false, 
+    'tarsFiles' => array(
+        './".$servantArr[0].$servantArr[1]."/".$servantFile."',
+    ),
+    'dstPath' => '../src/servant',
+    'namespacePrefix' => 'src\servant',
+);
+";
+        if($f = file_put_contents('../tars/'.$servantArr[0].$servantArr[1].'/tars.proto.php', $content,FILE_APPEND)){// 这个函数支持版本(PHP 5)
+            exit('写入tars.proto.php失败!');
+        }
+        $commond = 'cd ../tars && php ../src/vendor/phptars/tars2php/src/tars2php.php ./'.$servantArr[0].$servantArr[1].'/tars.proto.php';
+    }
+    public static function servantName($name)
+    {
+        if(strrchr($name,'.tars') == '.txt'){
+            return $name;
+        }else{
+            return $name.'.tars';
+        }
+    }
+    public static function validateServantPath($data)
+    {
+        $servantArr = explode('.',$data);
+        if(count($servantArr) != 3) {
+            return false;
+        }
+        if($servantArr[0] == '' || $servantArr[1] == '' || $servantArr[2] == '') {
+            return false;
+        }
+        return true;
+    }
 }
+DotarsInit::servant();
---
+++ tars/SearchServer/tars.proto.php
@@ -0,0 +1,13 @@
+
+<?php
+return array(
+    'appName' => 'Search',
+    'serverName' => 'Server',
+    'objName' => 'Obj',
+    'withServant' => false, 
+    'tarsFiles' => array(
+        './SearchServer/SearchServer.tars',
+    ),
+    'dstPath' => '../src/servant',
+    'namespacePrefix' => 'src\servant',
+);
